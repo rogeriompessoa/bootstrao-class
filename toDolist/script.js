b8af02c1f6883c40dd47534fae2fe7cb8b8d3a75
Charles <charles09111999@gmail.com>
2023-03-13 21:48:52 -0300

    teste

---
+++ toDolist/script.js
@@ -0,0 +1,17 @@
+let task = document.getElementById('task');
+
+console.log(task.value)
+
+
+function addTask(){
+    alert(`Adicionou: ${task.value}`)
+}
+
+function addTask() {
+
+let li = `< li class"list-group-item">${task.value}</li>`
+
+    let ul = document.getElementById('lista')
+}
+
+ul.innehtml= li
